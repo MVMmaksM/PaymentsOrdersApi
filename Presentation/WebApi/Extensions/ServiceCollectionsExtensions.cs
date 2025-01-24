78b17b1a36841efb143a9cc653266ac2d0d2f3dd
MVMmaksM <vaganov.maksim94@yandex.ru>
2025-01-24 22:32:24 +0500

    Add swagger

--- Presentation/WebApi/Extensions/ServiceCollectionsExtensions.cs
+++ Presentation/WebApi/Extensions/ServiceCollectionsExtensions.cs
@@ -1,15 +1,52 @@
-﻿namespace WebApi.Extensions
+﻿using Microsoft.OpenApi.Models;
+
+namespace WebApi.Extensions
 {
     public static class ServiceCollectionsExtensions
     {
         public static WebApplicationBuilder AddSwagger(this WebApplicationBuilder builder) 
         {
             builder.Services.AddSwaggerGen(option =>
-            option.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo 
             {
-                Description = "Payments orders API",
-                Version = "v1",
-            }));
+                option.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Web API",
+                    Description = "Payments orders API",
+                    Version = "v1",
+                    Contact = new OpenApiContact()
+                    {
+                        Email = "vaganov.maksim@mail.ru",
+                        Name = "Maksim",
+                        Url = new Uri("https://github.com/MVMmaksM/PaymentsOrdersApi")
+                    }
+                });
+
+                //настройка авторизации
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
 
             return builder;
         }
--- Presentation/WebApi/Program.cs
+++ Presentation/WebApi/Program.cs
@@ -8,26 +8,17 @@ namespace WebApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
+
             builder.AddSwagger();
 
+            var app = builder.Build();          
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
